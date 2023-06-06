1a1b2c4c59320abbc208c2fdbd0674bb999e8bc1
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2023-06-06 16:12:04 +0700

    api

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,8 +1,13 @@
 import { LayoutProps } from "@/models/index";
 import Link from "next/link";
-import * as React from "react";
+import React, { useEffect } from "react";
 
 export function MainLayout({ children }: LayoutProps) {
+  useEffect(() => {
+    console.log("MainLayout mounted");
+
+    return () => console.log("MainLayout unmounted");
+  }, []);
   return (
     <div>
       <h1>Main Layout</h1>
