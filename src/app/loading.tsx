654c91e7aa6fe2f2f68e8d64d34137d4e58393fb
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2023-06-08 23:29:22 +0700

    completed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
 export default function loading() {
   return (
-    <main className="bg-gray w-screen h-screen">
-      <h2 className=" m-auto text text-2xl font-bold">Loading...</h2>
+    <main className="bg-gray-600">
+      <div className="bg-gray-200 mx-auto max-w-2xl text-black py-2 min-h-screen">
+        <h2 className="m-4 text-2xl font-bold">Loading...</h2>
+      </div>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,9 @@ export default function Navbar() {
     <nav className="bg-white p-2 sticky top-0 drop-shadow-xl z-10">
       <div className="md:h-14 h-20 mx-auto md:px-4 container flex items-center justify-around flex-wrap md:flex-nowrap">
         <div className="text-indigo-500 font-bold text-xl md:order-1">
-          <h1>Kayden</h1>
+          <Link href="/">
+            <h1>Kayden</h1>
+          </Link>
         </div>
 
         <div className="order-2">
