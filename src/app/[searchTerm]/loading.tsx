dd2a9b42d29526e1a5d16ea7e14faef6130f37d4
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2023-06-08 23:22:39 +0700

    update loading

--- src/app/[searchTerm]/loading.tsx
+++ src/app/[searchTerm]/loading.tsx
@@ -1,7 +1,9 @@
 export default function loading() {
   return (
-    <main className="bg-slate-200 mx-auto max-w-lg p-1 min-h-screen">
-      <h2 className="m-4 text-2xl font-bold">Loading...</h2>
+    <main className="bg-gray-600">
+      <div className="bg-gray-200 mx-auto max-w-2xl text-black py-2 min-h-screen">
+        <h2 className="m-4 text-2xl font-bold">Loading...</h2>
+      </div>
     </main>
   );
 }
